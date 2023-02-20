07b5e32a20b3202d1b83557ff89683b7f47f19a6
Tyreik Williams <tyreik.mm.williams@gmail.com>
2023-02-20 10:58:43 -0800

    added winner popup - AB#1131

--- src/main/java/com/potionquest/gui/WinLoseMessage.java
+++ src/main/java/com/potionquest/gui/WinLoseMessage.java
@@ -1,34 +1,16 @@
 package com.potionquest.gui;
 
+
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
-public class PotionQuestGUILogic {
-
-    private static JFrame frame = null;
-
-    static {
-        try {
-            frame = new PotionQuestGUI();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+public class WinLoseMessage {
+    public static void main(String[] args) {
+        createWindow();
     }
 
-    public PotionQuestGUILogic() {
-        initGui();
-        frame.setVisible(true);
-        //createWindow();  //Winners window if clicked it should start new game
-        //code begins on line #31
-    }
-
-    public void initGui() {
-    }
-
-    //create a window saying winner
     private static void createWindow() {
-        JFrame frame = new JFrame("WINNER!");
+        JFrame frame = new JFrame("Swing Tester");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         createUI(frame);
@@ -37,11 +19,11 @@ public class PotionQuestGUILogic {
         frame.setVisible(true);
     }
 
-    private static void createUI(final JFrame frame) {
+    private static void createUI(final JFrame frame){
         JPanel panel = new JPanel();
         LayoutManager layout = new FlowLayout();
         panel.setLayout(layout);
-        JButton button = new JButton("You Win");
+            JButton button = new JButton("You Win");
         button.addActionListener(e -> JOptionPane.showMessageDialog(frame, "CLICK to play again!!",
                 "Play again", JOptionPane.QUESTION_MESSAGE));
 
